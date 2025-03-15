e7413d7011da977f025d6bf2cf084df206386203
Debottam Kar <debottamkar2003@gmail.com>
2025-03-15 15:15:58 +0530

    go backend

--- Backend/server.go
+++ Backend/server.go
@@ -5,13 +5,20 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// type User struct {
+// 	Name string
+// 	Age  int
+// }
+
 func main() {
 	var port string = ":5000"
-	app := fiber.New()
+	//app := fiber.New()
+	app := fiber.New(fiber.Config{Prefork: true})
 	fmt.Println("Server is running on port: ", port)
 	app.Get("/", func(c *fiber.Ctx) error {
-		fmt.Println("Request came successfully in my first go backend...")
-		return c.SendString("hello world")
+		//fmt.Println("Request came successfully in my first go backend...")
+		//user := User{Name: "Debottam Kar", Age: 22}
+		return c.SendString("Hello World!")
 	})
 	app.Listen(port)
 }
