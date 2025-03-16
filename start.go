968e05a8d5e4fcb8a5e842b9bec58350b70e0846
Debottam Kar <debottamkar2003@gmail.com>
2025-03-16 11:18:34 +0530

    structure

--- start.go
+++ start.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 	//"os"
 )
+type userProfile struct {
+    name string
+    age int
+}
+func (user userProfile) Display(id string) string{
+    return user.name + " " + fmt.Sprintf("%d", user.age) + " " + "id: " + id
+}
 func helloWorld() {
 	fmt.Println("hello world")
 }
@@ -31,4 +38,17 @@ func main() {
     // var arr = [4]int{7, 8, 9, 2}
     // fmt.Println(arr)
     helloWorld()
+    var user1 = userProfile{name: "Debottam", age: 21}
+    var user2 = userProfile{name: "Alice", age: 25}
+    var user3 = userProfile{name: "Bob", age: 30}
+
+    user1.name = "rony"
+    fmt.Println(user1)
+    fmt.Println(user1.Display("1"))
+
+    fmt.Println(user2)
+    fmt.Println(user2.Display("2"))
+
+    fmt.Println(user3)
+    fmt.Println(user3.Display("3"))
 }
