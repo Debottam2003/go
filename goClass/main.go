82eee72518fd206ac938087120e8ff3787a9f045
Debottam Kar <debottamkar2003@gmail.com>
2025-04-04 01:34:32 +0530

    nested structure

--- goClass/main.go
+++ goClass/main.go
@@ -3,7 +3,16 @@ package main
 import (
 	"fmt"
 )
-
+type Message struct {
+	Text string
+	Sender User
+	Receiver User
+}
+type User struct {
+	Username string
+}
 func main(){
 	fmt.Println("hello world")
+	var msg Message = Message{"This is a message", User{"Alice"}, User{"Bob"}}
+	fmt.Println(msg);
 }
