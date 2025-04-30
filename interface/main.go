2626dad505309fd4dc2adb2e6cbf514d6431f69a
Debottam Kar <debottamkar2003@gmail.com>
2025-05-01 01:22:58 +0530

    structure

--- interface/main.go
+++ interface/main.go
@@ -13,23 +13,23 @@ type Bmw struct {
 }
 
 func main() {
-	// fmt.Println("Hello, World!")
-	// // anonymous struct
-	// // anonymous struct is a struct without a name
-	// myuser := struct {
-	// 	name    string
-	// 	age     int8
-	// 	address struct { // added a field for the nested struct
-	// 		city string
-	// 	}
-	// }{
-	// 	"debottam",
-	// 	22,
-	// 	struct {
-	// 		city string
-	// 	}{"kolkata"},
-	// }
-	// fmt.Printf("User Name: %s, Age: %d, Address: %s\n", myuser.name, myuser.age, myuser.address.city)
+	fmt.Println("Hello, World!")
+	// anonymous struct
+	// anonymous struct is a struct without a name
+	myuser := struct {
+		name    string
+		age     int8
+		address struct { // added a field for the nested struct
+			city string
+		}
+	}{
+		"debottam",
+		22,
+		struct {
+			city string
+		}{"kolkata"},
+	}
+	fmt.Printf("User Name: %s, Age: %d, Address: %s\n", myuser.name, myuser.age, myuser.address.city)
 	var mycar Bmw = Bmw{Car: Car{brand: "BMW", model: "x5", year: 2023}, color: "black"}
 	fmt.Println(mycar.brand, mycar.model, mycar.year, mycar.color)
 	fmt.Println(mycar)
