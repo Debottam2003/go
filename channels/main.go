b0d1ffaca761a43a14b3dd787bf427ae2c5c4a69
Debottam Kar <debottamkar2003@gmail.com>
2025-05-11 12:04:44 +0530

    concurrency

--- channels/main.go
+++ channels/main.go
@@ -1,10 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func add(wg *sync.WaitGroup) {
+	defer wg.Done()
+	sum := 0
+	for i := 1; i <= 100; i++ {
+		sum += i
+	}
+	fmt.Println("sum:", sum);
+}
+
+func multiply(wg *sync.WaitGroup) {
+	defer wg.Done()
+	mul := 1
+	for i := 1; i <= 10; i++ {
+		mul *= i
+	}
+	fmt.Println("multiply:", mul);
+}
 
 func main() {
-	messsageChan := make(chan string)
-	messsageChan <- "ping"
-	msg := <- messsageChan
-	fmt.Println(msg)
+	// messsageChan := make(chan string)
+	// messsageChan <- "ping"
+	// msg := <- messsageChan
+	// fmt.Println(msg)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go add(&wg)
+	wg.Add(1)
+	go multiply(&wg)
+	wg.Wait()
 }
