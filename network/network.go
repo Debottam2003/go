7651203c4780c78c1063a16aa0c711da1ac7f933
Debottam Kar <debottamkar2003@gmail.com>
2025-05-11 21:05:14 +0530

    client-side data fetch (http.Get())

--- network/network.go
+++ network/network.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println(r.Method)
+//     fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// }
+
+// func main() {
+//     http.HandleFunc("/", handler)
+//     http.ListenAndServe(":8080", nil)
+// }
 
 func main() {
-	fmt.Println("Hello, World!")
+    // Fetching Data (Client Side)
+	resp, err := http.Get("http://localhost:5000/jokes")
+	if err != nil {
+		panic(err)
+	}
+    fmt.Println(*resp)
+    fmt.Println(resp.Status)
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+    fmt.Println(body)   
+    fmt.Println(string(body))
 }
