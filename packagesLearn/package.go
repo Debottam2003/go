47ad04d20fe5a33c804db7fa27f34f4d12e38f4d
Debottam Kar <debottamkar2003@gmail.com>
2025-11-17 00:28:29 +0530

    time package

--- packagesLearn/package.go
+++ packagesLearn/package.go
@@ -18,7 +18,25 @@ func main() {
 	fmt.Println("Current Minute:", t1.Minute())
 	fmt.Println("Current Second:", t1.Second())
 	fmt.Println("Unix Timestamp:", t1.Unix())
-	fmt.Println("Formatted Time:", t1.Format("2006-01-02 15:04:05"))
+	fmt.Println("Formatted Time:", t1.Format("2003-01-02 15:04:05"))
+
+	// time.Sleep(time.Second * 3)
+
+	str := "debottam kar it is"
+	// for _, v := range str {
+	// 	fmt.Print(string(v))
+	// 	time.Sleep(time.Second)
+	// }
+
+	fmt.Println(str)
+
+	for i := 0; i < len(str); i++ {
+		fmt.Print(string(str[i]))
+		time.Sleep(time.Second)
+	}
+
+	fmt.Println()
+
 	// Time package features
 	n1 := math.Abs(-3)
 	n2 := math.Floor(3.2)
